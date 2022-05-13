9b5b16806ac9fcc4c27cc8d052e8b4169d3aaa3f
JoseAndresGGiron <titogg@byui.edu>
2022-05-12 18:41:40 -0600

    Chamber of Commerce

---
+++ chamber/js/script.js
@@ -0,0 +1,43 @@
+let daynames=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+
+
+document.getElementById("lstmoddate").textContent = document.lastModified;
+document.getElementById("currentYear").textContent = year;
+document.getElementById("todaysDate").textContent = fulldate
+
+
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
