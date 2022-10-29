502c09b15e5aa896822bf7745fc5dafa58f6770d
JoseAndresGGiron <titogg@byui.edu>
2022-10-29 16:06:58 -0400

    working on my discover page

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -81,5 +81,19 @@ if('IntersectionObserver' in window) {
         loadImages(img);
     });
 }
+//Last visit
+
+const lastVisit = Number(localStorage.getItem('banner'));
+const currentVisit = Date.now() - lastVisit;
+const daysBetweenVisits = currentVisit / (1000 * 60 * 60 * 24);
+const firstVisit = `Welcome, this is your first visit!`
+const visits = `Welcome back! Your last visit was ${Math.round(daysBetweenVisits)} days ago.`
+
+if (lastVisit === 0) {
+    document.querySelector("#visits").textContent = firstVisit;
+} else {
+    document.querySelector("#visits").textContent = visits;
+}
+localStorage.setItem('lastvisit', Date.now())
 
 
