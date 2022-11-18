f2f3ffbad7415b50c7a23b7629d0b786acbc1286
JoseAndresGGiron <titogg@byui.edu>
2022-11-17 19:47:36 -0500

    doing updates on my files

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -6,11 +6,13 @@ const captionDesc = document.querySelector('figcaption');
 //Get the weather data from the API - apenweathermap.org
 const weatherAPI = "1c73d079411018bb31aa6c57e4ae946d";
 
-//htpp://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=
-//+502 `https://api.openweathermap.org/data/2.5/weather?id=${3598132}&units=imperial&lat=&lon=&appid=${weatherAPI}`
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//                           https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//API call for Fairbanks     htpp://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID={key#}
+//                          `htpp://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=${weatherAPI}`
+//Guatemala                 `https://api.openweathermap.org/data/2.5/weather?id=${3598132}&units=imperial&lat=&lon=&appid=${weatherAPI}`
+//                           https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
-fetch(`htpp://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=${weatherAPI}`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&lat=&lon=&appid=${weatherAPI}`)
     .then((response) => response.json())
     .then((data) => {
     console.log(data); // this is temporary for development onlys
